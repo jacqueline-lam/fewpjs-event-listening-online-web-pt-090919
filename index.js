9fc653c045cfe666bebd7387a5998d07059157ed
github username <github email address>
2020-03-25 20:14:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+  const input = document.getElementById('input');
+  //   event name to listen for, callback function 
+  input.addEventListener('click', function(event) {
+    alert('I was clicked!');
+  });
 }
+
